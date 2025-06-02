2c805f70fe26905bca4837e37ffd18c5981d6248
Laura Castro <lauracastrotech@gmail.com>
2025-06-02 15:34:22 -0400

    adds links and scripts:

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 "user strict"
+/**
+ * Logical Steps
+ * 
+ * What do we want to select? the buttons that control the temp and the update the landscape image
+ 
+ * Once we select the thing what do we do with it? if increase button is clicked the number should increase by one, if declrease button is clicked number should decres by 2, if current temp button is clicked API call to fetch temperature based on city name
+ 
+ * Does anything need to happen after we do what we want to do with the thing? 
+    The color must update based on temp ranges
+        Temperature (F)	Color
+        80+	Red
+        70-79	Orange  
+        60-69	Yellow
+        50-59	Green
+        49 or below	Teal
+    Landscape image should update 
+        Temperature (F)	Landscape
+        80+	"🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
+        70-79	"🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
+        60-69	"🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
+        59 or below	"🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
+
+        Pseudo code
+        variables: increase, decrease, temperature, color
+        use switch for colors
+        */
+const weather_report = () => {
+    CONST RED_LIMIT = 80
+    CONST ORANGE_LIMIT = 79
+    CONST YELLOW_LIMIT = 69
+    CONST GREEN_LIMIT = 59
+    CONST TEAL_LIMIT = 49
+
+    
+
+    // "orange" if 70 <= temperature >=79 else None  
+}
+
