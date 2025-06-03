abbad7e7356afb0ea786ceccba59c1f91c58bc1f
Laura Castro <lauracastrotech@gmail.com>
2025-06-03 15:13:30 -0400

    completes wave 2

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 "user strict"
-/**
- * Logical Steps
- * 
- * What do we want to select? the buttons that control the temp and the update the landscape image
- 
- * Once we select the thing what do we do with it? if increase button is clicked the number should increase by one, if declrease button is clicked number should decres by 2, if current temp button is clicked API call to fetch temperature based on city name
- 
- * Does anything need to happen after we do what we want to do with the thing? 
-    The color must update based on temp ranges
-        Temperature (F)	Color
-        80+	Red
-        70-79	Orange  
-        60-69	Yellow
-        50-59	Green
-        49 or below	Teal
-    Landscape image should update 
-        Temperature (F)	Landscape
-        80+	"🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
-        70-79	"🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
-        60-69	"🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
-        59 or below	"🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
-
-        Pseudo code
-        variables: increase, decrease, temperature, color
-        use switch for colors
-        */
+    // Landscape image should update 
+    //     Temperature (F)	Landscape
+    //     80+	"🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
+    //     70-79	"🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
+    //     60-69	"🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
+    //     59 or below	"🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
 
 const tempState = {
     currentTemp: 70
@@ -32,7 +12,6 @@ const tempState = {
 
 const updateTempColor = (temp) => {
     const tempAsInt = parseInt(tempState.currentTemp)
-    // const tempColor = document.getElementById("temp-number");
 
     if (tempAsInt >= 80) {
         temp.style.color = "red";
@@ -47,9 +26,24 @@ const updateTempColor = (temp) => {
     }
 };
 
+const updateLandscape = (temp) => {
+    const tempAsInt = parseInt(tempState.currentTemp)
+    const landscape = document.getElementById('landscape')
+    if (tempAsInt >= 80) {
+        landscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+    } else if (tempAsInt >= 70) {
+        landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
+    } else if (tempAsInt >= 60) {
+        landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"  
+    } else {
+        landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    };
+};
+
 const renderTemp = (temp) => {
     temp.textContent = tempState.currentTemp;
     updateTempColor(temp);
+    updateLandscape(temp);
     };
 
 const registerEventHandler = () => {
